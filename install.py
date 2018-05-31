ce2c943e58648ccc15cbb239a9dc9f73a6bc62c3
Mike <mshah.475@gmail.com>
2018-05-31 17:14:50 -0400

    A script to run that sets my dev environment.
    A script to run that should setup my development environment.

---
+++ install.py
@@ -0,0 +1,35 @@
+import os
+
+# Globals in case I need them
+PREFIX="sudo"
+INSTALLER="yum install"
+SUFFIX=""
+
+'''
+PREFIX Anything before the command
+INSTALLER Install command
+PROGRAM which program to install
+SUFFIX Any additional arguments (pipping, or perhaps using 'yes' to easily install)
+WHAT Any information as to what is being installed
+'''
+def yum_install(PREFIX,INSTALLER,PROGRAM,SUFFIX,WHAT):
+        print("=======Installing: "+PROGRAM+" =============")
+        print(WHAT)
+        os.system(PREFIX+" "+INSTALLER+" "+PROGRAM+" "+SUFFIX)
+
+
+yum_install("sudo","yum install","gdb","","A debugger for GCC compiled programs")
+yum_install("sudo","yum install","ddd","","Data Display Debugger is a GUI based debugger")
+
+
+'''
+        This function merely installs programs that I use on windows
+'''
+def Windows_Install():
+        print('Chrome')
+        print('Dreamweaver')
+        print('Acrobat Pro')
+        print('Notepad++')
+        print('Ubuntu Bash Shell')
+        print('Dropbox')
+        print('7zip')
