4b6dc4e1a8618508bfbfb9dc23b5438d030f75fd
Mike <mshah.475@gmail.com>
2024-08-29 20:39:07 -0400

    Update blenderSetup.sh

--- blenderSetup.sh
+++ blenderSetup.sh
@@ -2,10 +2,25 @@
 # https://wiki.blender.org/wiki/Building_Blender/Linux/Ubuntu
 
 # Install some common tools
-sudo apt install vim
-sudo apt install git
-sudo apt install make
+sudo apt update -Y
+sudo apt install -Y build-essentials
+sudo apt install -Y cmake
+sudo apt install -Y vim
+sudo apt install -Y tmux
+sudo apt install -Y git
+sudo apt install -Y tree
+sudo apt install -Y gdb
+sudo apt install -Y make
+sudo apt install -Y valgrind
+sudo apt install -Y cppcheck
+sudo apt install -Y perf
 
+# Install graphics libraries
+sudo apt install -Y libsdl2-dev
+
+# Install D Compiler
+wget https://downloads.dlang.org/releases/2.x/2.109.1/dmd_2.109.1-0_amd64.deb
+sudo dpkg -i dmd_2.109.1-0_amd64.deb
 
 # Install blender
 mkdir ~/blender-git
