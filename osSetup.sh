615a9dfad38e10764fb1af802511f82b401ab3b2
Mike <mshah.475@gmail.com>
2026-08-13 17:32:40 -0400

    Update osSetup.sh

--- osSetup.sh
+++ osSetup.sh
@@ -11,6 +11,7 @@ sudo apt install -y vim
 # set shiftwidth=4
 # set expandtab
 # set autoindent
+
 sudo apt install -y tmux
 sudo apt install -y git
 sudo apt install -y tree
@@ -18,6 +19,7 @@ sudo apt install -y gdb
 sudo apt install -y valgrind
 sudo apt install -y pkg-config
 sudo apt install -y curl
+sudo apt install -y entr
 
 # Web browser
 sudo apt install -y firefox
@@ -32,21 +34,30 @@ sudo apt install nasm -y
 sudo apt install -y cmake
 sudo apt install -y cppcheck
 sudo apt install -y cppman
+sudo apt install -y doxygen
 
 # Install graphics libraries
 sudo apt install -y libsdl3-dev
 sudo apt install -y mesa-utils
+sudo apt install -y glxinfo
+
+# OpenCL headers
+sudo apt install opencl-headers ocl-icd-opencl-dev -y
+sudo apt install clinfo -y
 
 # Windowing libraries (X11)
 # Install the manual pages
 sudo apt install libx11-doc -y
 
 # Install D Compiler
-wget https://downloads.dlang.org/releases/2.x/2.111.0/dmd_2.111.0-0_amd64.deb
-sudo dpkg -i dmd_2.111.0-0_amd64.deb
+wget https://downloads.dlang.org/pre-releases/2.x/2.113.0/dmd_2.113.0~rc.1-0_amd64.deb
+sudo dpkg -i dmd_2.113.0~rc.1-0_amd64.deb
+
+# Install zeal docs
+sudo apt install -y zeal
 
 # Note for arm based machines you'll want 'ldc' compiler
-# curl -fsS https://dlang.org/install.sh | bash -s ldc
+curl -fsS https://dlang.org/install.sh | bash -s ldc
 
 # Install the D language GDC compiler
 sudo apt-get install -y gdc-14
@@ -75,6 +86,12 @@ sudo apt-get install -y peek
 # Get blender dependencies
 # cd ~/blender-git
 #./blender/build_files/build_environment/install_deps.sh
+
+
+# Silly tool to get matrix background
+sudo apt install -y cmatrix
+# Useful tool for seeing visualization of hardware
+sudo apt install -y lstopo
 
 # Do a periodic update
 # make update
